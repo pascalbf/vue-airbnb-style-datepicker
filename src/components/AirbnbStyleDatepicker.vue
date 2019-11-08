688f24eea40ffec2e80d075a80c57ee82e2291de
Pascal Breton-Faguy <pascal.breton-faguy@libeo.com>
2019-11-08 11:24:02 -0500

    [FIX] Added props for width

--- src/components/AirbnbStyleDatepicker.vue
+++ src/components/AirbnbStyleDatepicker.vue
@@ -27,7 +27,7 @@
             <svg v-else viewBox="0 0 1000 1000">
               <path
                 d="M336.2 274.5l-210.1 210h805.4c13 0 23 10 23 23s-10 23-23 23H126.1l210.1 210.1c11 11 11 21 0 32-5 5-10 7-16 7s-11-2-16-7l-249.1-249c-11-11-11-21 0-32l249.1-249.1c21-21.1 53 10.9 32 32z"
-              ></path>
+              />
             </svg>
           </button>
         </div>
@@ -37,7 +37,7 @@
             <svg v-else viewBox="0 0 1000 1000">
               <path
                 d="M694.4 242.4l249.1 249.1c11 11 11 21 0 32L694.4 772.7c-5 5-10 7-16 7s-11-2-16-7c-11-11-11-21 0-32l210.1-210.1H67.1c-13 0-23-10-23-23s10-23 23-23h805.4L662.4 274.5c-21-21.1 11-53.1 32-32.1z"
-              ></path>
+              />
             </svg>
           </button>
         </div>
@@ -126,7 +126,7 @@
                     :style="getDayStyles(fullDate)"
                     @mouseover="() => { setHoverDate(fullDate) }"
                   >
-                  <button
+                    <button
                       class="asd__day-button"
                       type="button"
                       v-if="dayNumber"
@@ -245,6 +245,7 @@ export default {
     showShortcutsMenuTrigger: { type: Boolean, default: true },
     showMonthYearSelect: { type: Boolean, default: false },
     yearsForSelect: { type: Number, default: 10 },
+    baseWidth: { type: Number, default: 300 },
     isTest: {
       type: Boolean,
       default: () => process.env.NODE_ENV === 'test',
@@ -521,6 +522,10 @@ export default {
 
     if (this.startOpen || this.inline) {
       this.openDatepicker()
+    }
+
+    if (this.baseWidth) {
+      this.width = this.baseWidth;
     }
 
     this.$el.addEventListener('keyup', this.handleKeyboardInput)
